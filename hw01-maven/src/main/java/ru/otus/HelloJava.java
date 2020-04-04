52a13bf848640402538460f8026cbe1fa5e1e6e6
Teraule <diyakov@gmail.com>
2020-04-04 20:20:32 +0500

    fixes

--- hw01-maven/src/main/java/ru/otus/HelloJava.java
+++ hw01-maven/src/main/java/ru/otus/HelloJava.java
@@ -23,14 +23,4 @@ public class HelloJava {
       if (number > 25) System.out.print(number + " ");
     });
   }
-
-  private static void calcTime(Runnable runnable) {
-    long startTime = System.nanoTime();
-    for (int i = 0; i < MEASURE_COUNT; i++) {
-      runnable.run();
-    }
-    long finishTime = System.nanoTime();
-    long timeNs = (finishTime - startTime) / MEASURE_COUNT;
-    System.out.println("Time spent: " + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
-  }
 }
